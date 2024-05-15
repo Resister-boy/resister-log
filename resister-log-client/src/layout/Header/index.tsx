34eb653cf6567c07f24bb1cb72105cecaa90396e
resister-boy <hassanpumped17@gmail.com>
2024-05-15 23:49:30 +0900

    feat: add header component

--- resister-log-client/src/layout/Header/index.tsx
+++ resister-log-client/src/layout/Header/index.tsx
@@ -1,8 +1,20 @@
+'use client'
 import React from 'react'
+import styles from '@/layout/Header/header.module.scss'
+import classNames from 'classnames/bind'
+import Logo from '@/components/base/Logo'
+
+const cn = classNames.bind(styles)
 
 const Header = () => {
   return (
-    <header></header>
+    <header className={cn('container')}>
+      <div className={cn('inner')}>
+        <Logo
+          withImage={true}
+        />
+      </div>
+    </header>
   )
 }
 
